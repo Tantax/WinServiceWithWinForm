29df7b57aaa7edf91768dff268c77effcd21678f
Tantax <tantax-tang@foxmail.com>
2016-12-19 14:39:54 +0800

    log4net config setting modify
    1.  log file Path modify like "Log\\Info\\"
    2. Remind call "LogHelper.SetConfig()" mathod before using log4net

--- IAICN.GasFee.SendSMS.Service/Helper/LogHelper.cs
+++ IAICN.GasFee.SendSMS.Service/Helper/LogHelper.cs
@@ -9,37 +9,62 @@ using System.Threading.Tasks;
 
 namespace IAICN.GasFee.SendSMS.Service
 {
+    /// <summary>
+    /// Log4net使用帮助类
+    /// </summary>
     public class LogHelper
     {
-  
-        public static readonly ILog loginfo = LogManager.GetLogger("loginfo");   //选择<logger name="loginfo">的配置 
-   
-        public static readonly ILog logerror = LogManager.GetLogger("logerror");   //选择<logger name="logerror">的配置 
-   
-        public static void SetConfig()   
-        {   
-            XmlConfigurator.Configure();   
-        }   
-  
-        public static void SetConfig(FileInfo configFile)   
-        {   
-            XmlConfigurator.Configure(configFile);    
-        }   
-  
-        public static void WriteLog(string info)   
-        {   
-            if(loginfo.IsInfoEnabled)   
-            {   
-                loginfo.Info(info);   
-            }   
-        }   
-  
-        public static void WriteLog(string info,Exception se)   
-        {   
-            if(logerror.IsErrorEnabled)   
-            {   
-                logerror.Error(info,se);   
-            }   
-        } 
+
+        /// <summary>
+        /// 选择<logger name="loginfo">的配置 
+        /// </summary>
+        public static readonly ILog loginfo = LogManager.GetLogger("loginfo");
+
+        /// <summary>
+        /// 选择<logger name="logerror">的配置 
+        /// </summary>
+        public static readonly ILog logerror = LogManager.GetLogger("logerror");
+
+        /// <summary>
+        /// 配置初始化（一定要调用后才能使用日志）
+        /// </summary>
+        public static void SetConfig()
+        {
+            XmlConfigurator.Configure();
+        }
+
+        /// <summary>
+        /// 配置初始化
+        /// </summary>
+        /// <param name="configFile">配置文件</param>
+        public static void SetConfig(FileInfo configFile)
+        {
+            XmlConfigurator.Configure(configFile);
+        }
+
+        /// <summary>
+        /// 写入信息日志
+        /// </summary>
+        /// <param name="info"></param>
+        public static void WriteLog(string info)
+        {
+            if (loginfo.IsInfoEnabled)
+            {
+                loginfo.Info(info);
+            }
+        }
+
+        /// <summary>
+        /// 写入异常日志
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="se"></param>
+        public static void WriteLog(string info, Exception se)
+        {
+            if (logerror.IsErrorEnabled)
+            {
+                logerror.Error(info, se);
+            }
+        }
     }
 }
